dc11b6532a6ba82c63e2df5e250e8dd89ffb3ef8
Penchekrak <spiand97@gmail.com>
2021-12-02 15:11:25 +0300

    recursive determinant computation

--- sick.py
+++ sick.py
@@ -20,15 +20,28 @@ class SimpleDICK(nn.Module):
         nn.init.uniform_(self.horizontal_kernel, -init_bounds, init_bounds)
         nn.init.uniform_(self.vertical_kernel, -init_bounds, init_bounds)
 
-    def _assemble_W(self):
-        device = next(self.parameters()).device
+    @staticmethod
+    def recursive_log_det(kernel: torch.Tensor, size: int, blocks: int):
+        mx = kernel.abs().max()
+        b, a, c = kernel / mx
+        bc = b * c
+        det0 = 1
+        det1 = a
+        for i in range(2, size):
+            det0, det1 = det1, a * det1 - bc * det0
 
-        L = torch.tril(self.L, diagonal=-1) + torch.diag(torch.ones(self.dim)).to(device)
-        U = torch.triu(self.U, diagonal=1)
-        W = self.P @ L @ (U + torch.diag(self.S))
-        return W
+        log_det = blocks * (size * torch.log(mx) + torch.log(torch.abs(det1) + 1e-8))
+        # if not torch.isfinite(log_det):
+        #     print(b, a, c, bc, mx, torch.log(mx), torch.log(torch.abs(det1) + 1e-8))
+        #     det0 = 1
+        #     det1 = a
+        #     for i in range(2, size):
+        #         det0, det1 = det1, a * det1 - bc * det0
+        #         print(det1)
+        return log_det
 
     def forward(self, x):
+        _, _, h, w = x.shape
         x = F.conv2d(
             x,
             self.horizontal_kernel.unsqueeze(0).unsqueeze(0).unsqueeze(0),
@@ -38,8 +51,20 @@ class SimpleDICK(nn.Module):
             x, self.vertical_kernel.unsqueeze(-1).unsqueeze(0).unsqueeze(0),
             padding=(self.kernel_size // 2, 0)
         )
-        log_det = 1
+
+        # b, a, c = self.horizontal_kernel
+        # D = a**2 - 4 * b * c
+        # np1 = x.shape[-2] * x.shape[-1] + 1
+        # log_apd = torch.log(a + torch.sqrt(D))
+        # log_amd = torch.log(a - torch.sqrt(D))
+        # log_det = -0.5 * torch.log(D) + (n + 1) * math.log(2) + \
+        #           (n + 1) * log_apd + torch.log1p(-torch.exp((n + 1)))
+
+        log_det = SimpleDICK.recursive_log_det(self.vertical_kernel, h, w) + \
+                  SimpleDICK.recursive_log_det(self.horizontal_kernel, w, h)
+
         return x, log_det
 
-    def backward(self, z):
-        return z
+    def backward(self, x):
+
+        return x
--- tests.py
+++ tests.py
@@ -14,12 +14,23 @@ def test(f):
 @test
 def separable_equals_full():
     module = SimpleDICK(3)
-    tensor = torch.randn(1, 1, 5, 5)
-    x, log_det = module(tensor)
-
+    tensor = torch.randn(2, 1, 32, 32)
+    x, _ = module(tensor)
     full_kernel = torch.outer(module.vertical_kernel, module.horizontal_kernel)
     x_full = F.conv2d(tensor, full_kernel.unsqueeze(0).unsqueeze(0), padding=module.kernel_size // 2)
-    assert torch.allclose(x, x_full, atol=1e-15)
+    assert torch.allclose(x, x_full, atol=1e-7)
+
+@test
+def log_det_is_finite():
+    repetitions = 100
+    module = SimpleDICK(3)
+    for dimension in (2 ** i for i in range(4, 10)):
+        for r in range(repetitions):
+            tensor = torch.randn(2, 1, dimension, dimension)
+            _, log_det = module(tensor)
+            # if not torch.isfinite(log_det).all():
+            #     print(dimension, r, log_det.item())
+            assert torch.isfinite(log_det).all()
 
 
 if __name__ == "__main__":
